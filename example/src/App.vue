3c10c78422ad986170507034d6c61efb487c53a2
Karen <dev.kaskar2008@gmail.com>
2018-04-11 10:24:03 +0300

    boilerplate

---
+++ example/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <vue-simple-paginator>
+      <div slot-scope="{ _data }" id="asd">
+        <div slot="start"><pre>{{ _data }}</pre></div>
+      </div>
+    </vue-simple-paginator>
+  </div>
+</template>
+
+<script>
+  import VueSimplePaginator from '../../lib'
+
+  export default {
+    components: {
+      VueSimplePaginator
+    },
+    name: 'app'
+  }
+</script>
---
+++ lib/VueSimplePaginator.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .vue-simple-paginator
+    slot(v-bind="_self")
+      slot(name="prev")
+      slot(name="next")
+      slot(name="page-button")
+      slot(name="delimiter")
+      slot(name="start" v-bind="_self")
+      slot(name="end")
+</template>
+
+<script>
+  export default {
+    name: 'vue-simple-paginator',
+    data () {
+      return {
+        drp: 5
+      }
+    }
+  }
+</script>
